f174cc9ee16f6d6f1b1760e9bfe14f38a2e193e4
Reigns <122174090+reignsi@users.noreply.github.com>
2026-01-24 08:19:13 +0000

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks. 
+hi big man I 
+
+"""
 import os
 import sys
 
